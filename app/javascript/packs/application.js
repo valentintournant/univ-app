c98e47b2bf5a125acbda73c28ffdf8af01451b9f
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-23 14:15:39 +0100

    setup materalize me for rails six'

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
-
+import 'materialize-css/dist/js/materialize'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,3 +17,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 //= require materialize
+
+$(document).on('turbolinks:load', function() {
+  $(".dropdown-trigger").dropdown();
+});
