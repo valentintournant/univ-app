7df7b002bc9d6cbdd7fe573e166a0953d0a543be
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-23 16:42:44 +0100

    Front-end display and validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :short_name, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
