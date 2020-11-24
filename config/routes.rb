f090e9aee9ca2a65b1b6d4af6cbaa0a022beb154
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-24 16:06:16 +0100

    Authentication system - build routes and form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'courses/new', to: 'courses#new'
   resources :students, except: [:destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
