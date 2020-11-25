eb6dd4f034800e97068522dcec4f46716c0c623e
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-25 09:26:52 +0100

    Create and destroy sessions - add auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+helper_method :current_user, :logged_in?
+
+def current_user
+  #only perform this code if my @current_user exist
+  @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+end
+
+def logged_in?
+  #true or false
+  !!current_user
+end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    student = Student.find_by(email: params[:session][:email].downcase)
+    if student && student.authenticate(params[:session][:password])
+      session[:student_id] = student.id
+      flash[:notice] = "You have successfuly logged in"
+      redirect_to student
+    else
+      flash.now[:notice] = "Something was wrong with your login information"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:student_id] = nil
+    flash[:notice] = "You have successfuly logged out"
+    redirect_to root_path
   end
 
 end
